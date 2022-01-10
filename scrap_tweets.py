52a2f6bc6ee4eae4aca73803237b15a6484c3e34
botan <botan.david94@gmail.com>
2022-01-10 16:13:01 +0100

    Initial commit

---
+++ scrap_tweets.py
@@ -0,0 +1,74 @@
+import snscrape.modules.twitter as sntwitter
+from datetime import datetime
+import os
+import csv
+
+
+def scrap(query, file):
+    items = sntwitter.TwitterSearchScraper(query).get_items()
+
+    try:
+        os.remove(file)
+    except Exception:
+        print("No file to remove")
+
+    with open(file, mode='w', newline='') as file:
+        csv_writer = csv.writer(file, delimiter=';')
+
+        header = ['id', 'time', 'username', 'follower_count', 'friend_count', 'user_verified',
+                  'retweet_count', 'quote_count', 'like_count', 'reply_count', 'content']
+
+        csv_writer.writerow(header)
+        i = 0
+        for tweet in items:
+            user = tweet.user
+
+            features = [
+                tweet.id,
+                int(datetime.timestamp(tweet.date) * 1000),
+                user.username,
+                user.followersCount, user.friendsCount, user.verified,
+                tweet.retweetCount, tweet.quoteCount, tweet.likeCount, tweet.replyCount,
+                tweet.content.encode('utf-8')
+            ]
+            csv_writer.writerow(features)
+            i += 1
+
+            if i % 10_000 == 0:
+                print(i, "tweets downloaded")
+
+
+QUERIES = {
+    "Chiellini": ["Chiellini"],
+    "De Bruyne": ["De Bruyne"],
+    "Ruben Dias": ["Ruben Dias"],
+    "Donnarumma": ["Donnarumma"],
+    "Fernandes": ["Fernandes"],
+    "Foden": ["Foden"],
+    "Haaland": ["Haaland"],
+    "Jorginho": ["Jorginho"],
+    "Kane": ["Harry Kane", "Kane"],
+    "Kante": ["N'Golo Kante", "Kante", "Ngolo Kante"],
+    "Kjaer": ["Kjaer"],
+    "Lewandowski": ["Lewandowski"],
+    "Lukaku": ["Lukaku"],
+    "Mahrez": ["Mahrez"],
+    "Martinez": ["Lautaro Martinez"],
+    "Mbappe": ["Mbappe", "M'bappe"],
+    "Messi": ["Messi"],
+    "Modric": ["Modric"],
+    "Moreno": ["Moreno"],
+    "Mount": ["Mason Mount"],
+    "Neymar": ["Neymar"],
+    "Pedri": ["Pedri"],
+    "Ronaldo": ["Ronaldo", "CR7"],
+    "Salah": ["Mohammed Salah", "Salah", "Mohamed Salah"],
+    "Sterling": ["Sterling"],
+    "Suarez": ["Suarez"]
+}
+
+for query_key in QUERIES:
+    keywords = " ".join(QUERIES[query_key])
+    query = f"{keywords} lang:en since:2021-28-09 until:2021-28-11"
+
+    scrap(query, f"tweets/{query_key}.csv")
